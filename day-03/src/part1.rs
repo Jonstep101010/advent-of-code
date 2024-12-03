fe94ce8a52703071b2ba8eba4dabc7e816882f84
jschwabe <mail@jschwabe.site>
2024-12-03 20:08:26 +0100

    chore(day-03): beyond broken
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-03/src/part1.rs
+++ day-03/src/part1.rs
@@ -1,5 +1,7 @@
 // use nom::*;
 
+use nom::FindSubstring;
+
 fn parse(input: String) -> miette::Result<String> {
 	let start = input.find("mul(").unwrap();
 	let end = input.rfind(")").unwrap();
@@ -10,8 +12,93 @@ fn parse(input: String) -> miette::Result<String> {
 	// -> find "," and numbers enclosed
 	// mul(number1,number2)
 	// 0123       varies  varies
-	println!("");
-	Ok("".to_string())
+	let mut first = true;
+	let mut total = 0;
+	loop {
+		// if data.starts_with(")") {
+		// 	if data.as_bytes().len() == 1 {
+		// 		break;
+		// 	}
+		// 	data = &data[1..]
+		// }
+		if data.find("mul(").is_some() && data.find(")").is_some() {
+			let start = data.find("mul(").unwrap();
+			let comma_exists = data.find(",");
+			assert!(comma_exists.is_some());
+			let comma = comma_exists.unwrap();
+			let end = data.find(")").unwrap();
+			if start > end || comma + 1 > end || comma < start {
+				unreachable!("should not happen: start > end");
+			}
+			dbg!(&data[start + 4..comma]);
+			dbg!(&data[comma + 1..end]);
+			if data[start + 4..comma].parse::<i32>().is_ok()
+				&& data[comma..end].parse::<i32>().is_ok()
+			{
+				dbg!(data);
+				dbg!(&data[start..]);
+				dbg!(&data[start..end]);
+				let cur_elem = &data[start + 4..end];
+				dbg!(cur_elem);
+				// assert!(cur_elem.starts_with("mul("));
+				// assert!(cur_elem.ends_with(")"));
+				if !cur_elem.contains(",") {
+					dbg!(data);
+					continue;
+				}
+				// check that inside is num1,num2
+				dbg!(start);
+				dbg!(end);
+				// dbg!(&cur_elem[start..]);
+				let inside = cur_elem;
+				dbg!(inside);
+				let split_comma: Vec<&str> = inside.split(",").collect();
+				dbg!(&split_comma);
+				if split_comma.len() == 2 {
+					let first_result = split_comma[0].parse::<i32>();
+					let second_result = split_comma[1].parse::<i32>();
+					if first_result.is_ok() && second_result.is_ok() {
+						let to_calc = (
+							first_result.clone().unwrap(),
+							second_result.clone().unwrap(),
+						);
+						dbg!(&to_calc.0);
+						dbg!(&to_calc.1);
+						let mul_product = to_calc.0 * to_calc.1;
+						if first {
+							total = mul_product;
+							first = false;
+						} else {
+							total += mul_product;
+						}
+						let cur_slice = &data[end..];
+						let bind = cur_slice.find("mul(");
+						if bind.is_some() {
+							data = &cur_slice[bind.unwrap()..];
+						} else {
+							break;
+						}
+						dbg!(data);
+					}
+				} else {
+					let cur_slice = &data[end..];
+					let bind = cur_slice.find("mul(");
+					if bind.is_some() {
+						data = &cur_slice[bind.unwrap()..];
+					} else {
+						break;
+					}
+				}
+			} else {
+				data = &data[start..];
+				dbg!(data);
+			}
+		} else {
+			data = &data[start..];
+			dbg!(data);
+		}
+	}
+	Ok(total.to_string())
 }
 
 #[tracing::instrument]
@@ -20,9 +107,8 @@ pub fn process(input: &str) -> miette::Result<String> {
 	for line in input.lines() {
 		data.push_str(line);
 	}
-	let mut total = 0;
-	let parsed = parse(data);
-	Ok(total.to_string())
+	let parsed = parse(data)?;
+	Ok(parsed)
 }
 
 #[cfg(test)]
