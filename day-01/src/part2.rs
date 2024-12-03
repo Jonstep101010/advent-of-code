19c3d9137a451766ee01e864757851e6dbc1f926
jschwabe <mail@jschwabe.site>
2024-12-03 10:36:30 +0100

    chore(day-01): solution part2
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,6 +1,21 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-	todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+	let mut left = vec![];
+	let mut right = vec![];
+
+	for line in input.lines() {
+		let mut content = line.split_whitespace();
+		left.push(content.next().unwrap().parse::<i32>().unwrap());
+		right.push(content.next().unwrap().parse::<i32>().unwrap());
+	}
+	let mut score = 0;
+	// val = left[i]
+	for val in left.iter() {
+		// multiply val by amount of times it appears in right
+		// sum all vals
+		score += val * right.iter().filter(|&x| x == val).count() as i32;
+	}
+	Ok(score.to_string())
 }
 
 #[cfg(test)]
@@ -8,10 +23,14 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn test_process() -> miette::Result<()> {
-		todo!("haven't built test yet");
-		let input = "";
-		assert_eq!("", process(input)?);
+	fn test_task() -> miette::Result<()> {
+		let input = "3 4
+4 3
+2 5
+1 3
+3 9
+3 3";
+		assert_eq!("31", process(input)?);
 		Ok(())
 	}
 }
