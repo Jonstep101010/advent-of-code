27c638fab45844a088656181b0d590ecc26bc091
jschwabe <mail@jschwabe.site>
2024-12-09 00:49:39 +0100

    chore(day-08): parsing out chars done
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-08/src/part1.rs
+++ day-08/src/part1.rs
@@ -1,6 +1,24 @@
+use miette::miette;
+use nom::{
+	IResult, bytes::complete::take_till, character::complete::satisfy, multi::many1,
+	sequence::preceded,
+};
+
+fn parse(input: &str) -> IResult<&str, Vec<char>> {
+	let prec = preceded(
+		take_till(|c: char| nom::AsChar::is_alphanum(c)), // get rid of all these
+		satisfy(|c| nom::AsChar::is_alphanum(c)),         // keep all of this
+	);
+	many1(prec)(input)
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-	todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+	let (_input, parsing_result) =
+		parse(input).map_err(|err| miette!("failed to parse: {}", err))?;
+	dbg!(parsing_result);
+	let antinode_count = 0;
+	Ok(antinode_count.to_string())
 }
 
 #[cfg(test)]
@@ -9,7 +27,6 @@ mod tests {
 
 	#[test]
 	fn test_process() -> miette::Result<()> {
-		todo!("haven't built test yet");
 		let input = "............
 ........0...
 .....0......
