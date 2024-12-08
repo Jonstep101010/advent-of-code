efe574e43f60c4ba4a90344dbc3a907eb6b1749c
jschwabe <mail@jschwabe.site>
2024-12-08 02:06:35 +0100

    chore(day-06): remove dead code, use iterator in find_start
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-06/src/part2.rs
+++ day-06/src/part2.rs
@@ -1,35 +1,5 @@
 use std::collections::HashSet;
 
-fn find_start(grid: &[Vec<char>]) -> (usize, usize) {
-	for (y, row) in grid.iter().enumerate() {
-		for (x, &ch) in row.iter().enumerate() {
-			if ch == '^' {
-				return (y, x);
-			}
-		}
-	}
-	panic!("No starting position found");
-}
-
-#[allow(dead_code)]
-#[cfg(not(test))]
-fn print_grid(grid: &Vec<Vec<char>>) {
-	print!("\x1B[2J\x1B[1;1H");
-	for row in grid {
-		eprintln!("{}", row.iter().collect::<String>());
-	}
-	std::io::Write::flush(&mut std::io::stdout()).unwrap();
-}
-
-#[allow(dead_code)]
-#[cfg(test)]
-fn print_grid(grid: &Vec<Vec<char>>) {
-	eprintln!();
-	for row in grid {
-		eprintln!("{}", row.iter().collect::<String>());
-	}
-}
-
 const POSSIBLE_DIRECTIONS: [(i32, i32); 4] = [
 	(-1, 0), // up
 	(0, 1),  // right
@@ -37,6 +7,7 @@ const POSSIBLE_DIRECTIONS: [(i32, i32); 4] = [
 	(0, -1), // left
 ];
 
+#[inline]
 fn check_infinite_loop(start_pos: (usize, usize), grid: &[Vec<char>]) -> bool {
 	let mut visited_states: HashSet<((usize, usize), usize)> = HashSet::new();
 	let mut prev_position = start_pos;
@@ -84,10 +55,15 @@ fn walk_path(grid: &[Vec<char>]) -> ((usize, usize), HashSet<(usize, usize, usiz
 	let mut unique_positions: HashSet<(/* turns */ usize, usize, usize)> = HashSet::new();
 
 	// Start at ^ position (6,4)
+	fn find_start(grid: &[Vec<char>]) -> (usize, usize) {
+		grid.iter()
+			.enumerate()
+			.find_map(|(y, row)| row.par_iter().position_any(|&ch| ch == '^').map(|x| (y, x)))
+			.expect("No start position found")
+	}
 	let start_pos = find_start(grid);
 	let mut prev_position = start_pos;
 	unique_positions.insert((0, prev_position.0, prev_position.1));
-	let mut wall_positions: HashSet<(usize, usize)> = HashSet::new();
 	let mut turns = 0;
 
 	let mut cur_direction = POSSIBLE_DIRECTIONS[0];
@@ -112,10 +88,6 @@ fn walk_path(grid: &[Vec<char>]) -> ((usize, usize), HashSet<(usize, usize, usiz
 					turns += 1;
 				}
 			}
-			if (prev_y, prev_x) != start_pos {
-				traversed[prev_y][prev_x] = '+';
-			}
-			wall_positions.insert((y, x));
 			cur_direction = POSSIBLE_DIRECTIONS[turns];
 			let (ny, nx) = (
 				(prev_y as i32 + cur_direction.0) as usize,
