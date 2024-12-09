16906a8e812a755ae78510c1ebb07baf8f20d86e
jschwabe <mail@jschwabe.site>
2024-12-09 17:34:30 +0100

    chore(day-09): part1 get_checksum implemented
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-09/src/part1.rs
+++ day-09/src/part1.rs
@@ -1,8 +1,19 @@
+fn get_checksum(compressed: &str) -> usize {
+	let mut checksum = 0;
+	for (i, val) in compressed.chars().enumerate() {
+		if val.is_ascii_digit() {
+			checksum += i * (val.to_digit(10).unwrap() as usize);
+		}
+	}
+	checksum
+}
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
 	dbg!(input);
-	let result = 0;
-	Ok(result.to_string())
+	let compressed = todo!();
+	let checksum = get_checksum(compressed);
+	Ok(checksum.to_string())
 }
 
 #[cfg(test)]
@@ -13,6 +24,12 @@ mod tests {
 	fn test_process() -> miette::Result<()> {
 		let input = "2333133121414131402";
 		assert_eq!("1928", process(input)?);
+		Ok(())
+	}
+	#[test]
+	fn test_checksum() -> miette::Result<()> {
+		let input = "0099811188827773336446555566..............";
+		assert_eq!("1928", get_checksum(input).to_string());
 		Ok(())
 	}
 }
