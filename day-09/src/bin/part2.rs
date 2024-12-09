044c701a32c21c4a1f40673f052aed58398147f7
jschwabe <mail@jschwabe.site>
2024-12-09 18:32:11 +0100

    chore(day-09): formatting
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-09/src/bin/part2.rs
+++ day-09/src/bin/part2.rs
@@ -3,10 +3,10 @@ use miette::Context;
 
 #[tracing::instrument]
 fn main() -> miette::Result<()> {
-    tracing_subscriber::fmt::init();
+	tracing_subscriber::fmt::init();
 
-    let file = include_str!("../../input2.txt");
-    let result = process(file).context("process part 2")?;
-    println!("{}", result);
-    Ok(())
+	let file = include_str!("../../input2.txt");
+	let result = process(file).context("process part 2")?;
+	println!("{}", result);
+	Ok(())
 }
