8592fc0785e265287f5d1876fcbd3a6700fcfb67
jschwabe <mail@jschwabe.site>
2024-12-09 01:11:26 +0100

    chore(day-08): implement parsing with coordinates using IVec2 and Span using tutorial
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-08/src/part1.rs
+++ day-08/src/part1.rs
@@ -1,13 +1,27 @@
+use glam::IVec2;
 use miette::miette;
 use nom::{
-	IResult, bytes::complete::take_till, character::complete::satisfy, multi::many1,
+	AsChar, IResult, bytes::complete::take_till, character::complete::satisfy, multi::many1,
 	sequence::preceded,
 };
+use nom_locate::{LocatedSpan, position};
 
-fn parse(input: &str) -> IResult<&str, Vec<char>> {
+pub type Span<'a> = LocatedSpan<&'a str>;
+
+fn alphanum_position(input: Span) -> IResult<Span, (IVec2, char)> {
+	let (input, position) = position(input)?;
+	let (x, y) = (
+		position.get_column() as i32 - 1,
+		position.location_line() as i32 - 1,
+	);
+	let (input, c) = satisfy(|c| c.is_alphanum())(input)?;
+	Ok((input, (IVec2::new(x, y), c)))
+}
+
+fn parse(input: Span) -> IResult<Span, Vec<(IVec2, char)>> {
 	let prec = preceded(
 		take_till(|c: char| nom::AsChar::is_alphanum(c)), // get rid of all these
-		satisfy(|c| nom::AsChar::is_alphanum(c)),         // keep all of this
+		alphanum_position,                                // keep all of this
 	);
 	many1(prec)(input)
 }
@@ -15,7 +29,7 @@ fn parse(input: &str) -> IResult<&str, Vec<char>> {
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
 	let (_input, parsing_result) =
-		parse(input).map_err(|err| miette!("failed to parse: {}", err))?;
+		parse(Span::new(input)).map_err(|err| miette!("failed to parse: {}", err))?;
 	dbg!(parsing_result);
 	let antinode_count = 0;
 	Ok(antinode_count.to_string())
