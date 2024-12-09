d1fcbc24c9487c3a42602f3208d33e0ec6ed0503
jschwabe <mail@jschwabe.site>
2024-12-10 00:44:49 +0100

    chore(day-09): part1 done - ugly mess
    courtesy of reddit, python & genai
    
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-09/src/part1.rs
+++ day-09/src/part1.rs
@@ -1,84 +1,56 @@
-/*
-get_checksum:
-add up the result of multiplying each of these compressed blocks' position with the file ID number it contains.
-The leftmost block is in position 0. If a block contains free space, skip it instead
-
-get_checksum("022111222......") == 69
-get_checksum("0099811188827773336446555566..............") == 1928
-*/
-fn get_checksum(compressed: &str) -> usize {
-	let mut checksum = 0;
-	for (i, val) in compressed.chars().enumerate() {
-		if val.is_ascii_digit() {
-			checksum += i * (val.to_digit(10).unwrap() as usize);
-		}
-	}
-	checksum
+fn get_checksum(compressed_disk: &Vec<i64>) -> i64 {
+	compressed_disk
+		.iter()
+		.enumerate()
+		.map(|(i, &value)| i as i64 * value)
+		.sum()
 }
 
-fn parse_block(input: &str) -> String {
-	let mut block = String::new();
-	for (idx, val_idx) in input.chars().enumerate() {
-		// dbg!(val_idx);
-		let val = val_idx.to_digit(10);
-		if val.is_none() {
-			break;
-		}
-		let val = val.unwrap();
-		let sequence = if idx % 2 == 0 {
-			// number
-			let mut num_seq = String::new();
-			let to_push = if idx == 0 {
-				"0".to_string()
-			} else {
-				(idx / 2).to_string()
-			};
-			for _ in 0..val {
-				num_seq.push_str(&to_push);
-			}
-			num_seq
+fn parse_disk(input: &str) -> (Vec<i64>, Vec<usize>) {
+	let mut disk: Vec<i64> = Vec::new();
+	let mut file_id: i64 = 0;
+	for (i, c) in input.chars().enumerate() {
+		let x = c.to_digit(10).expect("Invalid digit") as usize;
+		if i % 2 == 0 {
+			// Append `x` copies of `fid` to `disk`
+			disk.extend(std::iter::repeat(file_id).take(x));
+			file_id += 1;
 		} else {
-			// dots
-			let mut num_seq = String::new();
-			for _ in 0..val {
-				num_seq.push('.');
-			}
-			num_seq
-		};
-		block.push_str(sequence.as_str());
+			// Append `x` copies of `-1` to `disk`
+			disk.extend(std::iter::repeat(-1).take(x));
+		}
 	}
-	block
+	let free_spaces: Vec<usize> = disk
+		.iter()
+		.enumerate()
+		.filter(|&(_, &value)| value == -1)
+		.map(|(index, _)| index)
+		.collect();
+	(disk, free_spaces)
 }
 
-// block of 0.110 needs to become 0101.
-fn compress(block: &str) -> String {
-	let mut compressed = block.to_owned();
-	loop {
-		// find position of first '.', make sure it is before last digit
-		let first_free_idx = compressed.find(".");
-		let last_num = compressed.rfind(|c: char| c.is_ascii_digit());
-		if first_free_idx.is_none() || first_free_idx >= last_num || last_num.is_none() {
+fn compress(disk: &mut Vec<i64>, free_spaces: Vec<usize>) -> &Vec<i64> {
+	// For each free index, replace with the last non-`-1` element
+	for &i in &free_spaces {
+		// Remove trailing `-1`s
+		while disk.last() == Some(&-1) {
+			disk.pop();
+		}
+		if disk.len() <= i {
 			break;
 		}
-		// swap contents
-		let mut ptrs = compressed.into_bytes();
-		let tmp = ptrs[last_num.unwrap()];
-		ptrs[last_num.unwrap()] = ptrs[first_free_idx.unwrap()];
-		ptrs[first_free_idx.unwrap()] = tmp;
-		compressed = String::from_utf8(ptrs).unwrap();
+		// Replace the free space with last element
+		disk[i] = disk.pop().expect("Disk is empty");
 	}
-	compressed
+	let compressed_disk = disk;
+	compressed_disk
 }
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-	// @audit only certain that example parsing is correct
-	dbg!(&input);
-	let block = parse_block(input);
-	dbg!(&block);
-	let compressed = compress(&block);
-	dbg!(&compressed);
-	let checksum = get_checksum(&compressed);
+	let (mut disk, free_spaces) = parse_disk(input);
+	let compressed_disk = compress(&mut disk, free_spaces);
+	let checksum: i64 = get_checksum(&compressed_disk);
 	Ok(checksum.to_string())
 }
 
@@ -93,37 +65,37 @@ mod tests {
 		assert_eq!("5", process("252")?);
 		assert_eq!("1928", process("2333133121414131402")?);
 		assert_eq!("12", process("111000000000000000001")?);
-		assert_eq!("69", process("12345")?);
+		assert_eq!("60", process("12345")?);
 		Ok(())
 	}
-	#[test]
-	fn test_parse() -> miette::Result<()> {
-		assert_eq!(
-			"00...111...2...333.44.5555.6666.777.888899".to_string(),
-			parse_block("2333133121414131402")
-		);
-		assert_eq!("0..111....22222".to_string(), parse_block("12345"));
-		assert_eq!("0.110".to_string(), parse_block("111000000000000000001"));
-		Ok(())
-	}
-	#[test]
-	fn test_compress() -> miette::Result<()> {
-		assert_eq!(
-			"0099811188827773336446555566..............".to_string(),
-			compress("00...111...2...333.44.5555.6666.777.888899")
-		);
-		assert_eq!(
-			"022111222......".to_string(),
-			compress(&"0..111....22222".to_string())
-		);
-		assert_eq!("0101.".to_string(), compress("0.110"));
-		Ok(())
-	}
-	#[test]
-	fn test_checksum() -> miette::Result<()> {
-		let input = "0099811188827773336446555566..............";
-		assert_eq!("1928", get_checksum(input).to_string());
-		assert_eq!("69", get_checksum("022111222......").to_string());
-		Ok(())
-	}
+	// #[test]
+	// fn test_parse() -> miette::Result<()> {
+	// 	assert_eq!(
+	// 		"00...111...2...333.44.5555.6666.777.888899".to_string(),
+	// 		parse_block("2333133121414131402")
+	// 	);
+	// 	assert_eq!("0..111....22222".to_string(), parse_block("12345"));
+	// 	assert_eq!("0.110".to_string(), parse_block("111000000000000000001"));
+	// 	Ok(())
+	// }
+	// #[test]
+	// fn test_compress() -> miette::Result<()> {
+	// 	assert_eq!(
+	// 		"0099811188827773336446555566..............".to_string(),
+	// 		compress("00...111...2...333.44.5555.6666.777.888899")
+	// 	);
+	// 	assert_eq!(
+	// 		"022111222......".to_string(),
+	// 		compress(&"0..111....22222".to_string())
+	// 	);
+	// 	assert_eq!("0101.".to_string(), compress("0.110"));
+	// 	Ok(())
+	// }
+	// #[test]
+	// fn test_checksum() -> miette::Result<()> {
+	// 	let input = "0099811188827773336446555566..............";
+	// 	assert_eq!("1928", get_checksum(input).to_string());
+	// 	assert_eq!("69", get_checksum("022111222......").to_string());
+	// 	Ok(())
+	// }
 }
