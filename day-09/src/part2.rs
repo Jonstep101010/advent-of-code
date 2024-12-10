bef9c0746fb4b3ceee8e1990a241f9df36291cab
jschwabe <mail@jschwabe.site>
2024-12-10 02:27:20 +0100

    chore(day-09): part2 finally done
    python solutions were better, inline_python next...; )
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-09/src/part2.rs
+++ day-09/src/part2.rs
@@ -1,55 +1,80 @@
-fn get_checksum(compressed_disk: &[i64]) -> i64 {
-	compressed_disk
-		.iter()
-		.enumerate()
-		.map(|(i, &value)| i as i64 * value)
-		.sum()
+#![warn(clippy::pedantic)]
+
+use std::collections::HashMap;
+
+fn get_checksum(files: &HashMap<usize, (usize, usize)>) -> usize {
+	let mut checksum: usize = 0;
+	for (fid, (pos, size)) in files {
+		for x in *pos..(*pos + *size) {
+			checksum += fid * x;
+		}
+	}
+	checksum
 }
 
-fn parse_disk(input: &str) -> (Vec<i64>, Vec<usize>) {
-	let mut disk: Vec<i64> = Vec::new();
-	let mut file_id: i64 = 0;
-	for (i, c) in input.trim().chars().enumerate() {
-		let x = c.to_digit(10).expect("Invalid digit") as usize;
+#[allow(clippy::type_complexity)]
+fn parse_files(input: &str) -> (HashMap<usize, (usize, usize)>, Vec<(usize, usize)>) {
+	let mut files = HashMap::new();
+	let mut free_spaces: Vec<(usize, usize)> = Vec::new();
+	let mut file_id: usize = 0;
+	let mut pos: usize = 0;
+	for (i, c) in input.chars().enumerate() {
+		let x = c.to_digit(10).unwrap() as usize;
 		if i % 2 == 0 {
-			// Append `x` copies of `fid` to `disk`
-			disk.extend(std::iter::repeat(file_id).take(x));
+			assert!(x != 0, "File size cannot be 0");
+			files.insert(file_id, (pos, x));
 			file_id += 1;
-		} else {
-			// Append `x` copies of `-1` to `disk`
-			disk.extend(std::iter::repeat(-1).take(x));
+		} else if x != 0 {
+			free_spaces.push((pos, x));
 		}
+		pos += x;
 	}
-	let free_spaces: Vec<usize> = disk
-		.iter()
-		.enumerate()
-		.filter(|&(_, &value)| value == -1)
-		.map(|(index, _)| index)
-		.collect();
-	(disk, free_spaces)
+	(files, free_spaces)
 }
 
-fn compress(disk: &mut Vec<i64>, free_spaces: Vec<usize>) -> &Vec<i64> {
-	// For each free index, replace with the last non-`-1` element
-	for i in free_spaces {
-		// Remove trailing `-1`s
-		while disk.last() == Some(&-1) {
-			disk.pop();
+// move in order of decreasing file_id number
+fn move_files(
+	file_ids: Vec<usize>,
+	mut files: HashMap<usize, (usize, usize)>,
+	free_spaces: &mut Vec<(usize, usize)>,
+) -> HashMap<usize, (usize, usize)> {
+	for file_id in file_ids {
+		let (pos, size) = files.get(&file_id).copied().unwrap();
+		let mut i = 0;
+		while i < free_spaces.len() {
+			let (fpos, fsize) = free_spaces[i];
+			if fpos >= pos {
+				free_spaces.truncate(i); // Remove extra free spaces
+				break;
+			}
+			if size <= fsize {
+				// Move file to free space
+				files.insert(file_id, (fpos, size));
+				if size == fsize {
+					free_spaces.remove(i);
+				} else {
+					free_spaces[i] = (fpos + size, fsize - size);
+				}
+				break;
+			}
+			i += 1;
 		}
-		if disk.len() <= i {
-			break;
-		}
-		// Replace the free space with last element
-		disk[i] = disk.pop().expect("Disk is empty");
 	}
-	disk
+	files
 }
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-	let (mut disk, free_spaces) = parse_disk(input);
-	let compressed_disk = compress(&mut disk, free_spaces);
-	let checksum: i64 = get_checksum(compressed_disk);
+	let (files, mut free_spaces) = parse_files(input.trim());
+	let file_ids: Vec<usize> = {
+		let mut file_id_keys: Vec<usize> = files.keys().copied().collect();
+		file_id_keys.sort_unstable_by(|a, b| b.cmp(a)); // Sort in reverse order
+		file_id_keys
+	};
+
+	let files = move_files(file_ids, files, &mut free_spaces);
+
+	let checksum = get_checksum(&files);
 	Ok(checksum.to_string())
 }
 
@@ -59,9 +84,6 @@ mod tests {
 
 	#[test]
 	fn test_process() -> miette::Result<()> {
-		// assert_eq!("16", process("14113")?);
-		// assert_eq!("6", process("133")?);
-		// assert_eq!("5", process("252")?);
 		assert_eq!("2858", process("2333133121414131402")?);
 		Ok(())
 	}
