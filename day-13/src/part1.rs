0ca9906a4abd8b9d7db7d92fc97296c0faa769ba
jschwabe <mail@jschwabe.site>
2024-12-13 23:09:02 +0100

    chore(day-13): part1 done - cleanup
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-13/src/part1.rs
+++ day-13/src/part1.rs
@@ -1,5 +1,4 @@
 use glam::UVec2;
-use itertools::Itertools;
 use miette::miette;
 use nom::{
 	IResult, Parser,
@@ -9,8 +8,6 @@ use nom::{
 	sequence::{preceded, separated_pair, terminated, tuple},
 };
 
-pub type TokenCost = u64;
-const MAX_BTN_PRESSES: u32 = 100;
 const COST_A: u32 = 3;
 const COST_B: u32 = 1;
 
@@ -19,19 +16,6 @@ struct ClawMachine {
 	a: UVec2,
 	b: UVec2,
 	prize: UVec2,
-}
-
-fn claw_prize(/* location information */) -> Option<TokenCost> {
-	let mut presses_a = 0;
-	let mut presses_b = 0;
-
-	if presses_a <= MAX_BTN_PRESSES && presses_b <= MAX_BTN_PRESSES {
-		// if possible in a, b <= 100
-		Some(presses_a as u64 + presses_b as u64)
-	} else {
-		// no possibility of reaching prize
-		None
-	}
 }
 
 fn parse_button(input: &str) -> IResult<&str, UVec2> {
@@ -84,28 +68,22 @@ fn parse<'a>(input: &'a str) -> IResult<&'a str, Vec<ClawMachine>> {
 	separated_list1(tuple((line_ending, line_ending)), game)(input)
 }
 
-#[tracing::instrument]
-pub fn process(input: &str) -> miette::Result<String> {
-	// parse input into usable data
-	// x and y for each button, prize location
-	// IVec2?
-	let (_, games) = parse(input).map_err(|err| miette!("invalid blocks in input: {}", err))?;
-	// use pathfinding to map with button combinations
-	// (max 100 presses per button)
-	// check which are possible: Some(token_cost), None (not possible)
-	// sum up all possible prizes' token cost
-
-	let results = games
+/// use pathfinding to map with button combinations
+/// (max 100 presses per button) - works without manual implementation
+/// check which are possible: Some(token_cost), None (not possible) - dijkstra handles this
+fn claw_prize_cost(games: &[ClawMachine]) -> Vec<u32> {
+	games
 		.iter()
-		.flat_map(|machine| {
-			// do something with djikstra algo
+		.filter_map(|machine| {
 			let result = pathfinding::prelude::dijkstra(
-				&UVec2::ZERO,
-				|position: &UVec2| {
-					let (x, y) = (position.x, position.y);
-					if x > machine.prize.x || y > machine.prize.y {
+				&UVec2::ZERO, // start pos
+				|&position| {
+					// set successors for goal_pos
+					if position.x > machine.prize.x || position.y > machine.prize.y {
+						// went past goal, non-success
 						vec![]
 					} else {
+						// walk path
 						vec![
 							(position + machine.a, COST_A),
 							(position + machine.b, COST_B),
@@ -114,23 +92,29 @@ pub fn process(input: &str) -> miette::Result<String> {
 				},
 				|prize| *prize == machine.prize,
 			);
-			// why is this not printing?
-			// dbg!(&result);
+			// why does this not need an unwrap?
 			result.map(|res_item| res_item.1)
 		})
-		.collect_vec();
+		.collect()
+}
 
-	dbg!(&results);
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+	// parse input into usable data
+	// x and y for each button, prize location - UVec2(x, y)
+	let (_, games) = parse(input).map_err(|err| miette!("invalid blocks in input: {}", err))?;
+
+	// token costs of possible paths in vector
+	let results = claw_prize_cost(&games);
 
 	// minimum tokens to get all possible prizes
+	// sum up all possible prizes' token cost
 	let required_tokens: u32 = results.iter().sum();
 	Ok(required_tokens.to_string())
 }
 
 #[cfg(test)]
 mod tests {
-	use miette::Error;
-
 	use super::*;
 
 	#[test]
@@ -161,13 +145,14 @@ Prize: X=8400, Y=5400";
 		assert_eq!("280", process(input)?);
 		Ok(())
 	}
-	// 	#[test]
-	// 	fn test_second_impossible() -> miette::Result<()> {
-	// 		// there are no possible combinations for reaching the prize
-	// 		let input = "Button A: X+26, Y+66
-	// Button B: X+67, Y+21
-	// Prize: X=12748, Y=12176";
-	// 		assert_eq!(mietteError, claw_prize(input)?);
-	// 		Ok(())
-	// 	}
+	#[test]
+	fn test_second_impossible() -> miette::Result<()> {
+		// there are no possible combinations for reaching the prize
+		let input = "Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+";
+		assert_eq!("0", process(input)?);
+		Ok(())
+	}
 }
