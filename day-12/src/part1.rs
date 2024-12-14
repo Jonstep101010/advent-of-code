5c7b89dc2f5e0ece66329936e0122d193840f618
jschwabe <mail@jschwabe.site>
2024-12-15 00:04:24 +0100

    chore(day-12): WIP part1 add graphmap from petgraph (tutorial)
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-12/src/part1.rs
+++ day-12/src/part1.rs
@@ -12,12 +12,33 @@ fn parse(input: &str) -> HashMap<(i32, i32), char> {
 		.collect()
 }
 
+// const directions
+const DIRECTIONS: [[i32; 2]; 4] = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+fn create_graph_directions(map: &HashMap<(i32, i32), char>) -> UnGraphMap<(i32, i32), ()> {
+	// see https://docs.rs/petgraph/latest/petgraph/graphmap/struct.GraphMap.html
+	let mut graph: GraphMap</* map */ (i32, i32), (), Undirected> = GraphMap::new();
+	for ((x, y), map_c) in map {
+		// create node from map
+		let node = graph.add_node((*x, *y));
+		for [dx, dy] in &DIRECTIONS {
+			// create node from direction
+			let new_node = (x + dx, y + dy);
+			if map.get(&new_node).is_some_and(|c2| map_c == c2) {
+				// add edge
+				graph.add_edge(node, new_node, ());
+			};
+		}
+	}
+	graph
+}
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
 	let map = parse(input);
-	dbg!(map);
-	// petgraph?
-	// const directions
+	dbg!(&map);
+	// petgraph? - condense graph
+	let graph = create_graph_directions(&map);
+	dbg!(&graph);
 	// area price = perimeter * amount inside perimeter
 	// sum of all area prices
 	let mut total_price = 0;
