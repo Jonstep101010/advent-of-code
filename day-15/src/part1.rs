d38fffedbcedbcfeeb04d20d46085d36267326e1
jschwabe <mail@jschwabe.site>
2024-12-15 22:23:55 +0100

    chore(day-15): part1 add base parsing
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-15/src/part1.rs
+++ day-15/src/part1.rs
@@ -1,6 +1,56 @@
+use grid::{Grid, GridRowIter, grid};
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Direction {
+	Up,
+	Right,
+	Down,
+	Left,
+}
+
+fn parse(input: &str) -> miette::Result<(Grid<char>, Vec<Direction>)> {
+	let (g, ins) = input.split_once("\n\n").unwrap();
+	// an x, y grid (row-major)
+	let mut grid = grid![];
+	for row in g.lines() {
+		let items = row.chars().collect_vec();
+		dbg!(&items);
+		grid.push_row(items);
+	}
+	dbg!(&grid);
+	let instructions = ins
+		.replace('\n', "")
+		.chars()
+		.map(|c| match c {
+			'^' => Direction::Up,
+			'>' => Direction::Right,
+			'v' => Direction::Down,
+			'<' => Direction::Left,
+			_ => panic!("Invalid direction character"),
+		})
+		.collect_vec();
+	Ok((grid, instructions))
+}
+
+// assert_eq!(grid[(5, 1)], 'O');
+// assert_eq!(grid[(4, 4)], '@');
+// // <vv>
+// dbg!(&movements[0..9]);
+// assert_eq!(
+// 	&[
+// 		Direction::Left,
+// 		Direction::Down,
+// 		Direction::Down,
+// 		Direction::Right,
+// 	],
+// 	&movements[0..=3] /* or 0..4 for first four */
+// );
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-	todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+	let (grid, movements) = parse(input)?;
+	todo!();
+	// Ok(box_checksum.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +59,28 @@ mod tests {
 
 	#[test]
 	fn test_process() -> miette::Result<()> {
-		todo!("haven't built test yet");
-		let input = "";
-		assert_eq!("", process(input)?);
+		let input = "##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
+		assert_eq!("10092", process(input)?);
 		Ok(())
 	}
 }
