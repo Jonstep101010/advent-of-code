c5dbfaa6fa2644273f5b0d7862be6f9da75e0052
jschwabe <mail@jschwabe.site>
2024-12-17 21:14:12 +0100

    chore(day-15): WIP part1 rewrite parsing
    Signed-off-by: jschwabe <mail@jschwabe.site>

--- day-15/src/part1.rs
+++ day-15/src/part1.rs
@@ -1,7 +1,71 @@
 use std::collections::HashSet;
 
-use grid::{Grid, grid, grid_cm};
+use grid::{Grid, grid};
 use itertools::Itertools;
+
+pub fn print_grid(grid: &mut Grid<char>) {
+	grid.rotate_left();
+	println!("grid:");
+	for row in grid.iter_rows() {
+		for &c in row {
+			print!("{}", c);
+		}
+		println!();
+	}
+	grid.rotate_right();
+}
+
+pub fn dbg_grid(grid: &mut Grid<char>) {
+	grid.rotate_left();
+	dbg!(&grid);
+	grid.rotate_right();
+}
+
+///
+/// Example:
+/// ```
+/// use day_15::part1::parse_grid;
+/// use day_15::part1::print_grid;
+/// use day_15::part1::dbg_grid;
+/// let input = "#######
+/// #...O..
+/// #......
+/// ";
+/// let mut grid = parse_grid(input);
+/// assert_eq!(grid.size(), (7, 3));
+/// assert_eq!(grid[(0, 2)], '#');
+/// assert_eq!(grid[(1, 2)], '#');
+/// assert_eq!(grid[(1, 1)], '.');
+/// assert_eq!(grid[(4, 1)], 'O');
+/// assert_eq!(grid[(3, 1)], '.');
+/// assert_eq!(grid[(5, 1)], '.');
+/// assert_eq!(grid[(4, 2)], '#');
+/// assert_eq!(grid[(0, 1)], '#');
+/// assert_eq!(grid[(6, 0)], '.');
+// /// grid.rotate_left();// can be used for printing
+// /// dbg!(&grid);// prints rotated to the right
+// /// grid.rotate_right();// reset
+/// print_grid(&mut grid);
+/// dbg_grid(&mut grid);
+/// ```
+pub fn parse_grid(input: &str) -> Grid<char> {
+	// for each line we want to have have a shelf
+	// where the later the line, the lower the shelf will be
+	// each line's first character should have an x of 0
+	let mut new = grid![];
+	for (lc, row) in input.lines().rev().enumerate() {
+		let items = row.chars().collect_vec();
+		new.push_row(items);
+		eprintln!("lc: {lc}");
+		for i in 0..row.len() {
+			dbg!(new[(lc, i)]);
+		}
+	}
+	dbg!(&new);
+	new.transpose();
+	dbg!(&new);
+	new
+}
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Direction {
@@ -22,60 +86,8 @@ impl Direction {
 	}
 }
 
-///
-/// Example:
-/// ```
-/// use day_15::part1::parse_grid;
-/// let input = "#######
-/// #...O..
-/// #......
-/// ";
-/// let grid = parse_grid(input);
-/// assert_eq!(grid.size(), (7, 3));
-/// assert_eq!(grid[(4, 1)], 'O');
-/// assert_eq!(grid[(3, 1)], '.');
-/// assert_eq!(grid[(5, 1)], '.');
-/// assert_eq!(grid[(4, 2)], '#');
-/// assert_eq!(grid[(0, 1)], '#');
-/// assert_eq!(grid[(6, 0)], '.');
-/// ```
-pub fn parse_grid(input: &str) -> Grid<char> {
-	// for each line we want to have have a shelf
-	// where the later the line, the lower the shelf will be
-	// each line's first character should have an x of 0
-	let mut new = grid![];
-	for (lc, row) in input.lines().rev().enumerate() {
-		let items = row.chars().collect_vec();
-		new.push_row(items);
-		eprintln!("lc: {lc}");
-		for i in 0..row.len() {
-			dbg!(new[(lc, i)]);
-		}
-	}
-	new.transpose();
-	new
-}
-
-fn parse(input: &str) -> miette::Result<(Grid<char>, Vec<Direction>)> {
-	let (g, ins) = input.split_once("\n\n").unwrap();
-	// an x, y grid (row-major)
-	let mut grid = grid![];
-	// @follow-up need to start from end and go back
-	for (lc, row) in g.lines().rev().enumerate() {
-		let items = row.chars().collect_vec();
-		dbg!(&items);
-		grid.push_row(items);
-		eprintln!("lc: {lc}");
-		for i in 0..row.len() {
-			dbg!(grid[(lc, i)]);
-		}
-	}
-	// grid is y top to bottom!
-	// grid.flip_rows();
-	grid.rotate_half();
-	// now cartesian :)
-	dbg!(&grid);
-	let instructions = ins
+fn parse_instructions(input: &str) -> Vec<Direction> {
+	input
 		.replace('\n', "")
 		.chars()
 		.map(|c| match c {
@@ -85,7 +97,13 @@ fn parse(input: &str) -> miette::Result<(Grid<char>, Vec<Direction>)> {
 			'<' => Direction::Left,
 			_ => panic!("Invalid direction character"),
 		})
-		.collect_vec();
+		.collect()
+}
+
+fn parse(input: &str) -> miette::Result<(Grid<char>, Vec<Direction>)> {
+	let (g, ins) = input.split_once("\n\n").unwrap();
+	let grid = parse_grid(g);
+	let instructions = parse_instructions(ins);
 	Ok((grid, instructions))
 }
 
@@ -223,50 +241,50 @@ mod tests {
 
 	#[test]
 	fn test_stuff() {}
-	#[test]
-	fn test_g1() {
-		let mut grid = grid![[1, 2, 3]
-[4, 5, 6]
-[7, 8, 9]];
-		grid.flip_cols();
-		assert_eq!(grid[(2, 0)], 9);
-		assert_eq!(grid.size(), (3, 3));
-		eprintln!("default");
-		dbg!(&grid);
-		assert_eq!(6, grid[(1, 2)]);
-		let grid = grid![[1,2,3][4,5,6][7,8,9]];
-		dbg!(&grid);
-		assert_eq!(grid[(1, 0)], 2);
-	}
-	#[test]
-	fn test_g2() {
-		// 777 is y_max / lines first
-		// 444 is in between them
-		// 111 is y_min / lines last
-		// we want to access as first the last lines
-		let mut grid = grid_cm![];
-		grid.insert_col(0, vec![7, 8, 9]);
-		grid.insert_col(0, vec![4, 5, 6]);
-		grid.insert_col(0, vec![1, 2, 3]);
-		// assert_eq!(grid, grid![[7,8,9][4,5,6][1,2,3]]);
-		// assert_eq!();
-		// y, x
-		assert_eq!(grid[(0, 1)], 2);
-		dbg!(&grid);
-		grid.transpose();
-		dbg!(&grid);
-		assert_eq!(grid[(1, 0)], 2);
-		dbg!(&grid);
-		grid.insert_row(3, vec![10]);
-		assert_eq!(grid.size(), (3, 3));
-	}
-	#[test]
-	fn test_contains() {
-		let mut walls = HashSet::new();
-		walls.insert(Pos(2, 4));
+	// 	#[test]
+	// 	fn test_g1() {
+	// 		let mut grid = grid![[1, 2, 3]
+	// [4, 5, 6]
+	// [7, 8, 9]];
+	// 		grid.flip_cols();
+	// 		assert_eq!(grid[(2, 0)], 9);
+	// 		assert_eq!(grid.size(), (3, 3));
+	// 		eprintln!("default");
+	// 		dbg!(&grid);
+	// 		assert_eq!(6, grid[(1, 2)]);
+	// 		let grid = grid![[1,2,3][4,5,6][7,8,9]];
+	// 		dbg!(&grid);
+	// 		assert_eq!(grid[(1, 0)], 2);
+	// 	}
+	// #[test]
+	// fn test_g2() {
+	// 	// 777 is y_max / lines first
+	// 	// 444 is in between them
+	// 	// 111 is y_min / lines last
+	// 	// we want to access as first the last lines
+	// 	let mut grid = grid_cm![];
+	// 	grid.insert_col(0, vec![7, 8, 9]);
+	// 	grid.insert_col(0, vec![4, 5, 6]);
+	// 	grid.insert_col(0, vec![1, 2, 3]);
+	// 	// assert_eq!(grid, grid![[7,8,9][4,5,6][1,2,3]]);
+	// 	// assert_eq!();
+	// 	// y, x
+	// 	assert_eq!(grid[(0, 1)], 2);
+	// 	dbg!(&grid);
+	// 	grid.transpose();
+	// 	dbg!(&grid);
+	// 	assert_eq!(grid[(1, 0)], 2);
+	// 	dbg!(&grid);
+	// 	grid.insert_row(3, vec![10]);
+	// 	assert_eq!(grid.size(), (3, 3));
+	// }
+	// #[test]
+	// fn test_contains() {
+	// 	let mut walls = HashSet::new();
+	// 	walls.insert(Pos(2, 4));
 
-		assert!(walls.contains(&Pos(2, 4)));
-	}
+	// 	assert!(walls.contains(&Pos(2, 4)));
+	// }
 
 	#[test]
 	fn test_process() -> miette::Result<()> {
@@ -296,16 +314,11 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 	}
 	#[test]
 	fn test_grid() {
-		// let input = "#######
-		// #...O..
-		// #......
-
-		// <>";
 		// let (mut grid, _) = parse(input).unwrap();
 		let mut grid_rev_stack = grid_cm![
-			['.', '.', '.', '.', '.', '.', '#']
-			['.', '.', 'O', '.', '.', '.', '#']
-			['#', '#', '#', '#', '#', '#', '#']
+		['.', '.', '.', '.', '.', '.', '#']
+		['.', '.', 'O', '.', '.', '.', '#']
+		['#', '#', '#', '#', '#', '#', '#']
 		];
 		// y, x
 		assert_eq!(grid_rev_stack[(1, 2)], 'O');
@@ -322,6 +335,12 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 		// y, x
 		assert_eq!(grid_two[(1, 4)], 'O');
 		dbg!(&grid_two);
+		let input = "#######
+#...O..
+#......
+";
+		let grid = parse_grid(input);
+		assert_eq!(grid_two, grid);
 		// assert_eq!(expected_grid[(2, 1)], grid[(2, 1)]);
 	}
 	#[test]
