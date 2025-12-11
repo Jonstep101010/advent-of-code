c24bc1f4446e9d5ee130c384083a9f2ce1cde129
Jonstep101010 <git@jschwabe.site>
2025-12-11 19:45:36 +0100

    day2part1: cleanup python
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-02/part1.py
+++ 2025/day-02/part1.py
@@ -5,21 +5,17 @@ if len(sys.argv) > 1:
 else:
 	INPUT = "input1.txt"
 
+invalid_ids = 0
 with open(INPUT) as f:
 	ranges = f.read().split(",")
-	ranges_nums = []
 	for cur_range in ranges:
 		rangesplit = cur_range.split("-")
-		cur_range = [rangesplit[0], rangesplit[1]]
-		ranges_nums.append(range(int(rangesplit[0]), int(rangesplit[1]) + 1))
-	invalid_ids = 0
-	for num_range in ranges_nums:
-		for i in num_range:
+		for i in range(int(rangesplit[0]), int(rangesplit[1]) + 1):
 			numstr = str(i)
 			mid = int(len(numstr) / 2)
-			left, right = numstr[0:mid], numstr[mid:]
+			left, right = numstr[:mid], numstr[mid:]
 			if left == right:
-				print(numstr[0:mid], numstr[mid:])
+				print(numstr[:mid], numstr[mid:])
 				invalid_ids += i
 
 print(invalid_ids)
