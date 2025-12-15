b8dd519edee8c0c33ecefa56d5210bcb29bebfa6
Jonstep101010 <git@jschwabe.site>
2025-12-15 21:07:07 +0100

    day3part2: rust reduce
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-03/src/part2.rs
+++ 2025/day-03/src/part2.rs
@@ -1,25 +1,42 @@
-use itertools::Itertools;
+#[allow(dead_code)]
+fn max_joltage_set_by_key(battery_bank: &str) -> String {
+	use itertools::Itertools;
+	let mut joltages = String::with_capacity(12);
+	let mut offset = 0;
+	for i in 0..12 {
+		let (idx, first_max) = &battery_bank[offset..(battery_bank.len() - 11 + i)]
+			.chars()
+			.enumerate()
+			.max_set_by_key(|(_, battery)| *battery)
+			.first()
+			.cloned()
+			.unwrap();
+		joltages.push(*first_max);
+		offset += idx + 1;
+	}
+	joltages
+}
 
+fn max_joltage_reduce(battery_bank: &str) -> String {
+	let mut joltages = String::with_capacity(12);
+	let mut offset = 0;
+	for i in 0..12 {
+		let (idx, first_max) = &battery_bank[offset..(battery_bank.len() - 11 + i)]
+			.chars()
+			.enumerate()
+			.reduce(|acc, next| if next.1.gt(&acc.1) { next } else { acc })
+			.unwrap();
+		joltages.push(*first_max);
+		offset += idx + 1;
+	}
+	joltages
+}
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
 	let joltages_total = _input
 		.lines()
-		.map(|battery_bank| {
-			let mut joltages: Vec<char> = vec![];
-			let mut offset = 0;
-			for i in 0..12 {
-				let (idx, first_max) = &battery_bank[offset..(battery_bank.len() - 11 + i)]
-					.chars()
-					.enumerate()
-					.max_set_by_key(|(_, battery)| *battery)
-					.first()
-					.cloned()
-					.unwrap();
-				joltages.push(*first_max);
-				offset += idx + 1;
-			}
-			joltages.iter().collect::<String>().parse::<u64>().unwrap()
-		})
+		.map(|battery_bank| max_joltage_reduce(battery_bank).parse::<u64>().unwrap())
+		// .map(|battery_bank| max_joltage_set_by_key(battery_bank).parse::<u64>().unwrap())
 		.sum::<u64>();
 	Ok(joltages_total.to_string())
 }
