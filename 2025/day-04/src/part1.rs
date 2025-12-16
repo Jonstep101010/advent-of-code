e1643a5204fd74545cbef411c16988fbe0751998
Jonstep101010 <git@jschwabe.site>
2025-12-16 12:28:25 +0100

    day4part1: WIP rust
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-04/src/part1.rs
+++ 2025/day-04/src/part1.rs
@@ -1,8 +1,36 @@
+use std::collections::HashSet;
+
+use glam::IVec2;
+const DIRECTIONS: [IVec2; 8] = [
+	IVec2::NEG_X,
+	IVec2::NEG_Y,
+	IVec2::X,
+	IVec2::Y,
+	IVec2::ONE,
+	IVec2::NEG_ONE,
+	IVec2::new(1, -1),
+	IVec2::new(-1, 1),
+];
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-	let lines = _input.lines();
-	let linecount = lines.count();
-	dbg!(&linecount);
+	let roll_positions = _input
+		.lines()
+		.enumerate()
+		.map(|(y, line)| {
+			line.chars().enumerate().filter_map(move |(x, c)| {
+				if c == '@' {
+					// dbg!(&x, &y);
+					Some(IVec2::new(x as i32, y as i32))
+				} else {
+					None
+				}
+			})
+		})
+		.flatten()
+		.collect::<HashSet<IVec2>>();
+	// let linecount = lines.count();
+	// dbg!(&linecount);
 	// parse into array with locations of IVec2
 	let paper_rolls_accessible = 0;
 	Ok(paper_rolls_accessible.to_string())
