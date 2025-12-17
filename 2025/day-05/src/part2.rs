27462525767333abae08d4a4ed58c65228ca68c4
Jonstep101010 <git@jschwabe.site>
2025-12-17 21:56:22 +0100

    day5part2: WIP rust
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-05/src/part2.rs
+++ 2025/day-05/src/part2.rs
@@ -1,6 +1,56 @@
+use std::ops::RangeBounds;
+
+use itertools::{Itertools, any};
+use miette::IntoDiagnostic;
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-	todo!("day 01 - part 2");
+	let mut fresh_ranges = vec![];
+	let mut lines = _input.lines();
+	while let Some(line) = lines.next()
+		&& !line.is_empty()
+	{
+		let split = line.split("-").collect_vec();
+		let range = std::ops::RangeInclusive::new(
+			split[0].parse::<usize>().into_diagnostic()?,
+			split[1].parse::<usize>().into_diagnostic()?,
+		);
+		fresh_ranges.push(range);
+	}
+	// dbg!(&fresh_ranges);
+	let min_start = fresh_ranges
+		.iter()
+		.map(|range_lol| {
+			// something
+			*range_lol.start()
+		})
+		.min()
+		.unwrap();
+	let max_end = fresh_ranges
+		.iter()
+		.map(|range_lol| {
+			// something
+			*range_lol.end()
+		})
+		.max()
+		.unwrap();
+	let mut contained = max_end + 1 - min_start;
+	for i in min_start..=max_end {
+		if !any(&fresh_ranges, |range_fresh| range_fresh.contains(&i)) {
+			contained -= 1;
+		}
+	}
+
+	// dbg!(&ingredients);
+	// let total_fresh = ingredients
+	// 	.into_iter()
+	// 	.filter(|ingredient| {
+	// 		itertools::any(&fresh_ranges, |fresh_range| {
+	// 			fresh_range.contains(&ingredient)
+	// 		})
+	// 	})
+	// 	.count();
+	Ok(contained.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +59,18 @@ mod tests {
 
 	#[test]
 	fn test_process() -> miette::Result<()> {
-		todo!("haven't built test yet");
-		let input = "";
-		assert_eq!("", process(input)?);
+		let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+		assert_eq!("14", process(input)?);
 		Ok(())
 	}
 }
