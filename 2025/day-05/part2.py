b52c2a6aed94cb55968fb1425a65a5e98741d8ea
Jonstep101010 <git@jschwabe.site>
2025-12-18 20:56:03 +0100

    day5part2: python faster
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-05/part2.py
+++ 2025/day-05/part2.py
@@ -6,13 +6,23 @@ else:
 	INPUT = "input2.txt"
 
 with open(INPUT) as f:
-	rangeset = set()
+	ranges = []
 	while True:
 		line = f.readline()
-		if line == "\n":
+		if not line or line == "\n":
 			break
 		split = [int(x) for x in line.split("-")]
-		start, end = split[0], split[1]
-		for n in range(start, end + 1):
-			rangeset.add(n)
-	print(len(rangeset))
+		ranges.append((split[0], split[1]))
+
+	ranges.sort()
+
+	count = 0
+	start, end = ranges[0]
+	for range_start, range_end in ranges[1:]:
+		if range_start > end + 1:
+			count += end - start + 1
+			start, end = range_start, range_end
+		end = max(end, range_end)
+
+	count += end - start + 1
+	print(count)
