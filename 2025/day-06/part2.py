690696183c11dc5111feab9c6449a12da738d3ad
Jonstep101010 <git@jschwabe.site>
2025-12-19 22:50:07 +0100

    day6part2: python from scratch ffs
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-06/part2.py
+++ 2025/day-06/part2.py
@@ -1,5 +1,5 @@
 import sys
-from operator import mul, add
+from math import prod
 
 if len(sys.argv) > 1:
 	INPUT = sys.argv[1]
@@ -7,97 +7,46 @@ else:
 	INPUT = "input2.txt"
 errored = []
 with open(INPUT) as f:
-	line = f.readline().strip()
-	calcs = []
-	ops = None
-	while True:
-		split = line.split()
-		if "+" in line or "*" in line or "-" in line or "/" in line:
-			break
-		else:
-			calcs.extend([x for x in split])
-		line = f.readline().strip()
-	ops = split
-	# n_vecs = len(ops)
-	# print(ops)
-	# print(calcs)
-	mcalc = []
+	# stupid ass shit WITH spaces
+	lines = [line.strip("\n") for line in f]
+	max_len = max(len(line) for line in lines)
+
+	# padding to ensure safe access
+	rows = [line.ljust(max_len) for line in lines]
+
+	ops = list(rows.pop())
+
 	total = 0
-	for i, op in enumerate(ops):
-		op = mul if op == "*" else add
-		mcalc.append([calcs[i]])
-		for ii in range(len(ops) + i, len(calcs), len(ops)):
-			mcalc[i].append(calcs[ii])
-		# print(mcalc[i])
-		# print(" -  -  - ")
-		ii = 0
-		cur = []
-		for nstr in mcalc[i]:
-			arr = [int(x[-1]) for x in reversed(nstr)]
-			for x in nstr:
-				ii += 1
-			cur.append((arr))
-		print(cur)
+	newdigits = []
+	alldigits = []
+	for col in reversed(range(len(ops))):
+		digits = ""
+		for row in rows:
+			digit = row[col]
+			if digit == " ":
+				digits += ""
+			else:
+				digits += digit
+		if digits == "":
+			newdigits = []
+		else:
+			newdigits.append(int(digits))
+		op = ops[col]
+		if op == "*":
+			total += prod(newdigits)
+			alldigits.append(newdigits)
+		elif op == "+":
+			total += sum(newdigits)
+			alldigits.append(newdigits)
+		elif op == " ":
+			pass
 
-		newdigits = []
-		# cur = [[4, 6], [2, 3], [4, 1, 3]]
-		newd_pos = 0
-		while True:
-			# print(digits)
-			try:
-				max_index = max(range(len(cur)), key=lambda idx: len(cur[idx]))
-			except ValueError:
-				break
-			ii = 0
-			maxnum = ""
-			while cur != []:
-				if [] in cur:
-					cur.remove([])
-				print(f"cur {cur}")
-				try:
-					print(f"maxnum: {cur[max_index][ii]}")
-					maxnum += str(cur[max_index][ii])
-					print(f"max: {cur[max_index]}")
-					cur[max_index] = cur[max_index][1:]
-				except IndexError:
-					print(f"idxerr: {cur}")
-					cur = []
-					break
-				if max_index == len(cur) - 1:
-					print(f"appending to array {int(maxnum)}")
-					newdigits.append(int(maxnum))
-					newd_pos += 1
-					maxnum = ""
-					max_index = 0
-					# break
-				elif cur[max_index] == []:
-					continue
-				else:
-					print(f"incrementing on {cur} with maxindex = {max_index}")
-					max_index += 1
-			print(f"newds = {newdigits}")
-			if not (
-				newdigits == [356, 24, 1]
-				or newdigits == [4, 431, 623]
-				or newdigits == [175, 581, 32]
-				or newdigits == [8, 248, 369]
-			):
-				print(f"error in {newdigits}")
-				errored.append(newdigits)
-			# ([['3', '2', '1'], ['5', '4'], ['6']],)
-			# -> longest len first: 3
-			# -> continue from top to bottom on lower lenghts: 5, 6
-			# ['2', '1'], ['4'] -> 2, 4
-			# ['1']
-
-		# res = 356 * 24 * 1 = 8544
-		# print(res)
-		# total += res
-if errored != []:
-	print("error in these")
-	for e in errored:
-		print(e)
-print(total)
-
-
+if not (
+	[356, 24, 1] in alldigits
+	and [4, 431, 623] in alldigits
+	and [175, 581, 32] in alldigits
+	and [8, 248, 369] in alldigits
+):
+	print(f"error in {alldigits}")
 # expected: 1058 + 3253600 + 625 + 8544 = 3263827
+print(total)
