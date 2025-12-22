75ee537e7caba467985167ffdf2aa323bcfb4363
Jonstep101010 <git@jschwabe.site>
2025-12-22 19:36:37 +0100

    day6part1: rust basic
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-06/src/part1.rs
+++ 2025/day-06/src/part1.rs
@@ -1,6 +1,39 @@
+use itertools::Itertools;
+use miette::IntoDiagnostic;
+
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-	todo!("day 01 - part 1");
+	let mut nums = vec![];
+	let mut ops: Option<Vec<&str>> = None;
+	for line in _input.lines() {
+		let split = line.split_whitespace();
+		if itertools::any(line.chars(), |c| c == '*' || c == '+') {
+			ops = Some(split.collect_vec());
+			break;
+		} else {
+			split.for_each(|str| {
+				let n = str.parse::<usize>().into_diagnostic().expect("valid n");
+				nums.push(n);
+			});
+		}
+	}
+	let mut calcs: Vec<Vec<usize>> = vec![];
+	let mut total: usize = 0;
+	let opslen = ops.clone().expect("operator trailing").iter().len();
+	for (i, op) in ops.expect("trailing ops").into_iter().enumerate() {
+		dbg!(&calcs);
+		calcs.push(vec![nums[i]]);
+		dbg!(&calcs);
+		(opslen + i..nums.len())
+			.step_by(opslen)
+			.for_each(|ii| calcs[i].push(nums[ii]));
+		total += if op == "*" {
+			calcs[i].iter().product::<usize>()
+		} else {
+			calcs[i].iter().sum::<usize>()
+		};
+	}
+	Ok(total.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +42,12 @@ mod tests {
 
 	#[test]
 	fn test_process() -> miette::Result<()> {
-		todo!("haven't built test yet");
-		let input = "";
-		assert_eq!("", process(input)?);
+		let input = "123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
+";
+		assert_eq!("4277556", process(input)?);
 		Ok(())
 	}
 }
