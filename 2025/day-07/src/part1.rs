c6b498d29946ea59295246e4a62f9dc16dbf4724
Jonstep101010 <git@jschwabe.site>
2025-12-23 18:24:57 +0100

    day7part1: rust basic
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-07/src/part1.rs
+++ 2025/day-07/src/part1.rs
@@ -1,6 +1,30 @@
+use std::collections::HashSet;
+
+use itertools::Itertools;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-	todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+	let manifold_pos = input
+		.lines()
+		.enumerate()
+		.find(|(_y, line)| line.contains('S'))
+		.map(|(y, line)| line.find('S').and_then(|x| Some((x, y))))
+		.flatten();
+	let mut beam_positions: HashSet<(usize, usize)> = HashSet::new();
+	debug_assert!(beam_positions.insert(manifold_pos.unwrap()));
+	let mut splits = 0;
+	for (y, line) in input.lines().enumerate() {
+		for (x, c) in line.chars().enumerate() {
+			if c == '^' && beam_positions.contains(&(x, y - 1)) {
+				beam_positions.insert((x - 1, y));
+				beam_positions.insert((x + 1, y));
+				splits += 1;
+			} else if beam_positions.contains(&(x, y.wrapping_sub(1))) {
+				beam_positions.insert((x, y));
+			}
+		}
+	}
+	Ok(splits.to_string())
 }
 
 #[cfg(test)]
