e49b4bc949e8a8ed1a93f1c9109642641cbfb518
Jonstep101010 <git@jschwabe.site>
2025-12-23 17:43:36 +0100

    day7part1: python basic formatting
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-07/part1.py
+++ 2025/day-07/part1.py
@@ -10,30 +10,18 @@ with open(INPUT) as f:
 	beam_positions = set()
 	for y, line in enumerate(filestr):
 		if "S" in line:
-			manifold_pos = line.index("S")
-			beam_positions.add((manifold_pos, y + 1))
+			beam_positions.add((line.index("S"), y + 1))
 			break
-	# print(filestr)
-	test = 0
+	new_beam_insertions = 0
 	for y, line in enumerate(filestr):
 		for x, c in enumerate(line):
 			if c == "^" and (x, y - 1) in beam_positions:
-				test += 1
+				new_beam_insertions += 1
 				beam_positions.add((x - 1, y))
 				beam_positions.add((x + 1, y))
 			elif (x, y - 1) in beam_positions:
 				beam_positions.add((x, y))
-	print(beam_positions)
-	print(len(beam_positions))
-
-	# beam_order = []
-	# for pos in beam_positions:
-	# 	beam_order.append(pos)
-	# beam_order = sorted(beam_order)
-
-	# print()
-	# print(beam_order)
-	print(test)
+	print(new_beam_insertions)
 
 # for each splitter, a tachyon beam can at most be split into two
 # position below the splitter cannot have beam unless there are two splitters next to each other
