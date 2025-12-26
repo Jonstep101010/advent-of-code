b7e0055fd3e67a8e1ef01f842186e7250bcaeb6d
Jonstep101010 <git@jschwabe.site>
2025-12-26 14:21:55 +0100

    day7(rust): formatting
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-07/src/part1.rs
+++ 2025/day-07/src/part1.rs
@@ -4,29 +4,34 @@ use std::collections::HashSet;
 pub fn process(input: &str) -> miette::Result<String> {
 	let mut lines_it = input.lines().enumerate();
 	let manifold_pos = lines_it
-		.find(|(_y, line)| line.contains('S'))
+		.next()
 		.and_then(|(y, line)| line.find('S').map(|x| (x, y)))
 		.unwrap();
 	let beam_positions: HashSet<(usize, usize)> = HashSet::from([manifold_pos]);
 	let (splits, _) = lines_it.fold(
 		(0, beam_positions),
 		|(splits, mut beam_positions), (y, line)| {
-			let line_splits = line
-				.chars()
-				.enumerate()
-				.filter_map(|(x, c)| {
-					beam_positions.contains(&(x, y.wrapping_sub(1))).then(|| {
-						if c == '^' {
-							(beam_positions.insert((x - 1, y)) | beam_positions.insert((x + 1, y)))
-								as usize
-						} else {
-							beam_positions.insert((x, y));
-							0
-						}
-					})
-				})
-				.sum::<usize>();
-			(splits + line_splits, beam_positions)
+			(
+				splits
+					+ line
+						.chars()
+						.enumerate()
+						.filter_map(|(x, c)| {
+							match beam_positions.contains(&(x, y.wrapping_sub(1))) {
+								true if c == '^' => Some(usize::from(
+									beam_positions.insert((x - 1, y))
+										| beam_positions.insert((x + 1, y)),
+								)),
+								true => {
+									beam_positions.insert((x, y));
+									None
+								}
+								_ => None,
+							}
+						})
+						.sum::<usize>(),
+				beam_positions,
+			)
 		},
 	);
 	Ok(splits.to_string())
--- 2025/day-07/src/part2.rs
+++ 2025/day-07/src/part2.rs
@@ -4,13 +4,13 @@ use std::collections::HashMap;
 pub fn process(input: &str) -> miette::Result<String> {
 	let mut lines_it = input.lines().enumerate();
 	let manifold_pos = lines_it
-		.find(|(_y, line)| line.contains('S'))
-		.and_then(|(y, line)| line.find('S').map(|x| (x, y)))
+		.next()
+		.and_then(|(_, line)| line.chars().position(|c| c == 'S'))
 		.unwrap();
 
 	Ok(lines_it
 		.fold(
-			HashMap::from([(manifold_pos.0, 1); 1]),
+			HashMap::from([(manifold_pos, 1); 1]),
 			|hm_acc_pos, (_y, line)| {
 				let mut new_pos: HashMap<usize, usize> = HashMap::default();
 				hm_acc_pos.iter().for_each(|(&x, &count)| {
