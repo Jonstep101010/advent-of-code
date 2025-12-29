7f81e502e44b2a7b106a2015bc5a47622ef2e549
Jonstep101010 <git@jschwabe.site>
2025-12-29 14:30:14 +0100

    day8part2: python with tutorial
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-08/part2.py
+++ 2025/day-08/part2.py
@@ -5,6 +5,50 @@ if len(sys.argv) > 1:
 else:
 	INPUT = "input2.txt"
 
+unions_dict = {}
+
+
+class Union:
+	def __init__(self, members):
+		self.members = members
+		for m in members:
+			unions_dict[m] = self
+
+	def union(self, other):
+		return Union(self.members | other.members)
+
+
 with open(INPUT) as f:
-	filestr = f.read()
-	print(filestr)
+	pos = []
+	for line in f.readlines():
+		p = tuple(int(axis) for axis in line.split(","))
+		pos.append(p)
+		Union({p})
+
+	all_pairs = []
+	for i, p in enumerate(pos):
+		for ii in range(i + 1, len(pos)):
+			q = pos[ii]
+			all_pairs.append((p, q))
+	all_pairs.sort(
+		key=lambda pair: sum(
+			(axis[0] - axis[1]) ** 2 for axis in zip(pair[0][:3], pair[1][:3])
+		)
+	)
+	# MAX_CONNECTIONS = (
+	# 	1000 if INPUT == "input2.txt" else 10
+	# )
+	for i, (p, q) in enumerate(all_pairs):
+		# if i == MAX_CONNECTIONS:
+		# 	unions = set()
+		# 	for p in pos:
+		# 		unions.add(unions_dict[p])
+		# 	unions = list(sorted(unions, key=lambda u: len(u.members), reverse=True))
+		# 	print(
+		# 		len(unions[0].members) * len(unions[1].members) * len(unions[2].members)
+		# 	)
+		if unions_dict[p] != unions_dict[q]:
+			u = unions_dict[p].union(unions_dict[q])
+			if len(u.members) == len(pos):
+				print(p[0] * q[0])
+				break
