0338dcae0b66b838edf544c3a58b52dde5fc7def
Jonstep101010 <git@jschwabe.site>
2025-12-29 14:55:57 +0100

    day8: python refactor/rewrite
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-08/part1.py
+++ 2025/day-08/part1.py
@@ -5,45 +5,74 @@ if len(sys.argv) > 1:
 else:
 	INPUT = "input1.txt"
 
-unions_dict = {}
 
+class UnionFind:
+	def __init__(self):
+		self.parent = {}
+		self.rank = {}
 
-class Union:
-	def __init__(self, members):
-		self.members = members
-		for m in members:
-			unions_dict[m] = self
+	def add(self, x):
+		"""Add a new element to the union-find structure."""
+		if x not in self.parent:
+			self.parent[x] = x
+			self.rank[x] = 0
 
-	def union(self, other):
-		return Union(self.members | other.members)
+	def __find(self, x):
+		"""Find the root of x with path compression."""
+		if self.parent[x] != x:
+			self.parent[x] = self.__find(self.parent[x])
+		return self.parent[x]
+
+	def union(self, x, y) -> bool:
+		"""Union two sets by rank. Returns True if they were merged."""
+		root_x = self.__find(x)
+		root_y = self.__find(y)
+
+		if root_x == root_y:
+			return False
+
+		if self.rank[root_x] < self.rank[root_y]:
+			self.parent[root_x] = root_y
+		elif self.rank[root_x] > self.rank[root_y]:
+			self.parent[root_y] = root_x
+		else:
+			self.parent[root_y] = root_x
+			self.rank[root_x] += 1
+		return True
+
+	def get_groups(self) -> dict:
+		"""Return a dict mapping roots to lists of members."""
+		groups = {}
+		for x in self.parent:
+			root = self.__find(x)
+			if root not in groups:
+				groups[root] = []
+			groups[root].append(x)
+		return groups
 
 
 with open(INPUT) as f:
+	uf = UnionFind()
 	pos = []
 	for line in f.readlines():
 		p = tuple(int(axis) for axis in line.split(","))
 		pos.append(p)
-		Union({p})
+		uf.add(p)
 
 	all_pairs = []
 	for i, p in enumerate(pos):
 		for ii in range(i + 1, len(pos)):
 			q = pos[ii]
-			all_pairs.append((p, q))
-	all_pairs.sort(
-		key=lambda pair: sum(
-			(axis[0] - axis[1]) ** 2 for axis in zip(pair[0][:3], pair[1][:3])
-		)
-	)
-	MAX_CONNECTIONS = 1000 if INPUT == "input1.txt" else 10  # for example output of 40
-	for i, (p, q) in enumerate(all_pairs):
-		if i == MAX_CONNECTIONS:
-			unions = set()
-			for p in pos:
-				unions.add(unions_dict[p])
-			unions = list(sorted(unions, key=lambda u: len(u.members), reverse=True))
-			print(
-				len(unions[0].members) * len(unions[1].members) * len(unions[2].members)
-			)
+			dist = sum((axis[0] - axis[1]) ** 2 for axis in zip(p[:3], q[:3]))
+			all_pairs.append((dist, p, q))
+	all_pairs.sort()
+	MAX_CONNECTIONS = 1000 if INPUT == "input1.txt" else 10
+	pairs_processed = 0
+	for dist, p, q in all_pairs:
+		uf.union(p, q)
+		pairs_processed += 1
+		if pairs_processed == MAX_CONNECTIONS:
+			groups = uf.get_groups()
+			sizes = sorted([len(members) for members in groups.values()], reverse=True)
+			print(sizes[0] * sizes[1] * sizes[2])
 			break
-		unions_dict[p].union(unions_dict[q])
--- 2025/day-08/part2.py
+++ 2025/day-08/part2.py
@@ -5,25 +5,58 @@ if len(sys.argv) > 1:
 else:
 	INPUT = "input2.txt"
 
-unions_dict = {}
 
+class UnionFind:
+	def __init__(self):
+		self.parent = {}
+		self.rank = {}
+		self.size = {}
 
-class Union:
-	def __init__(self, members):
-		self.members = members
-		for m in members:
-			unions_dict[m] = self
+	def add(self, x):
+		"""Add a new element to the union-find structure."""
+		if x not in self.parent:
+			self.parent[x] = x
+			self.rank[x] = 0
+			self.size[x] = 1
 
-	def union(self, other):
-		return Union(self.members | other.members)
+	def __find(self, x):
+		"""Find the root of x with path compression."""
+		if self.parent[x] != x:
+			self.parent[x] = self.__find(self.parent[x])
+		return self.parent[x]
+
+	def union(self, x, y) -> bool:
+		"""Union two sets by rank, updating sizes. Returns True if they were merged."""
+		root_x = self.__find(x)
+		root_y = self.__find(y)
+
+		if root_x == root_y:
+			return False
+
+		if self.rank[root_x] < self.rank[root_y]:
+			self.parent[root_x] = root_y
+			self.size[root_y] += self.size[root_x]
+		elif self.rank[root_x] > self.rank[root_y]:
+			self.parent[root_y] = root_x
+			self.size[root_x] += self.size[root_y]
+		else:
+			self.parent[root_y] = root_x
+			self.size[root_x] += self.size[root_y]
+			self.rank[root_x] += 1
+		return True
+
+	def get_size(self, x) -> int:
+		"""Get the size of the set containing x."""
+		return self.size[self.__find(x)]
 
 
 with open(INPUT) as f:
+	uf = UnionFind()
 	pos = []
 	for line in f.readlines():
 		p = tuple(int(axis) for axis in line.split(","))
 		pos.append(p)
-		Union({p})
+		uf.add(p)
 
 	all_pairs = []
 	for i, p in enumerate(pos):
@@ -35,20 +68,10 @@ with open(INPUT) as f:
 			(axis[0] - axis[1]) ** 2 for axis in zip(pair[0][:3], pair[1][:3])
 		)
 	)
-	# MAX_CONNECTIONS = (
-	# 	1000 if INPUT == "input2.txt" else 10
-	# )
-	for i, (p, q) in enumerate(all_pairs):
-		# if i == MAX_CONNECTIONS:
-		# 	unions = set()
-		# 	for p in pos:
-		# 		unions.add(unions_dict[p])
-		# 	unions = list(sorted(unions, key=lambda u: len(u.members), reverse=True))
-		# 	print(
-		# 		len(unions[0].members) * len(unions[1].members) * len(unions[2].members)
-		# 	)
-		if unions_dict[p] != unions_dict[q]:
-			u = unions_dict[p].union(unions_dict[q])
-			if len(u.members) == len(pos):
+
+	total_points = len(pos)
+	for p, q in all_pairs:
+		if uf.union(p, q):
+			if uf.get_size(p) == total_points:
 				print(p[0] * q[0])
 				break
