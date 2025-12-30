a3df27261fc6f3a4ca4b04b161cddddd5c20b1a7
Jonstep101010 <git@jschwabe.site>
2025-12-30 16:44:59 +0100

    day9part1: python basic
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-09/part1.py
+++ 2025/day-09/part1.py
@@ -1,4 +1,5 @@
 import sys
+import itertools
 
 if len(sys.argv) > 1:
 	INPUT = sys.argv[1]
@@ -6,5 +7,14 @@ else:
 	INPUT = "input1.txt"
 
 with open(INPUT) as f:
-	filestr = f.read()
-	print(filestr)
+	pos = []
+	for line in f.readlines():
+		pos.append([int(line_it) for line_it in line.split(",")])
+	print(pos)
+
+	areas = []
+	for pair in itertools.combinations(pos, 2):
+		areas.append(
+			(abs(pair[0][0] - pair[1][0]) + 1) * (abs(pair[0][1] - pair[1][1]) + 1)
+		)
+	print(max(areas))
