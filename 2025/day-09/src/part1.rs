f5c3790f65cb8d4206361a0f4fd88132e20b4c3a
Jonstep101010 <git@jschwabe.site>
2025-12-30 14:13:06 +0100

    day9: WIP rust
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-09/src/part1.rs
+++ 2025/day-09/src/part1.rs
@@ -1,22 +1,41 @@
 use glam::IVec2;
 
+fn square(p: &IVec2, q: &IVec2) -> u64 {
+	let tmp = u64::from((p.x.abs_diff(q.x) + 1) * (p.y.abs_diff(q.y) + 1));
+	dbg!(tmp)
+}
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
 	let (_, red_tile_pos) = parse(input).expect("parse success");
-	dbg!(red_tile_pos);
+	// dbg!(&red_tile_pos);
 	// find biggest/smallest x and y
 	// calculate size from max difference in each axis
-	Ok(0.to_string())
+	// biggest is in between 2/5 and 11/1
+	let max_area = red_tile_pos
+		.iter()
+		.tuple_combinations()
+		.map(|(a, b)| {
+			square(a, b)
+			// dbg!(a, b);
+			// some max calc
+		})
+		.max()
+		.unwrap();
+	Ok(max_area.to_string())
 }
 
 use itertools::Itertools;
-use miette::IntoDiagnostic;
 use nom::{IResult, Parser, character::complete::line_ending, multi::separated_list1};
 fn parse(input: &str) -> IResult<&str, Vec<IVec2>> {
 	separated_list1(
 		line_ending,
-		separated_list1(nom::bytes::tag(","), nom::character::complete::i32)
-			.map(|v| IVec2::from_slice(&v)),
+		nom::sequence::separated_pair(
+			nom::character::complete::i32,
+			nom::bytes::tag(","),
+			nom::character::complete::i32,
+		)
+		.map(|(x, y)| IVec2::new(x, y)),
 	)
 	.parse(input)
 }
