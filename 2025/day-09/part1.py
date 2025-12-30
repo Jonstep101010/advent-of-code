0f1ec2bce622aec3874db93f9c4d44218741f411
Jonstep101010 <git@jschwabe.site>
2025-12-30 16:51:11 +0100

    day9part1: python
    Signed-off-by: Jonstep101010 <git@jschwabe.site>

--- 2025/day-09/part1.py
+++ 2025/day-09/part1.py
@@ -7,14 +7,13 @@ else:
 	INPUT = "input1.txt"
 
 with open(INPUT) as f:
-	pos = []
-	for line in f.readlines():
-		pos.append([int(line_it) for line_it in line.split(",")])
-	print(pos)
+	positions = [tuple(int(axis) for axis in line.split(",")) for line in f.readlines()]
+	# print(pos)
 
-	areas = []
-	for pair in itertools.combinations(pos, 2):
-		areas.append(
-			(abs(pair[0][0] - pair[1][0]) + 1) * (abs(pair[0][1] - pair[1][1]) + 1)
-		)
-	print(max(areas))
+	max_area = max(
+		[
+			(abs(p[0] - q[0]) + 1) * (abs(p[1] - q[1]) + 1)
+			for p, q in itertools.combinations(positions, 2)
+		]
+	)
+	print(max_area)
